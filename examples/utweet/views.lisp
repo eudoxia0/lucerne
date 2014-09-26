78b3d09bdbf6988609f158b830d668c13063f17e
Fernando Borretti <eudoxiahp@gmail.com>
2014-09-26 20:23:56 -0300

    Add µtweet views

---
+++ examples/utweet/views.lisp
@@ -0,0 +1,101 @@
+(in-package :cl-user)
+(defpackage utweet.views
+  (:use :cl :lucerne :crane :utweet.models)
+  (:import-from :lucerne-auth
+                :get-userid
+                :login
+                :logout
+                :logged-in-p))
+(in-package :utweet.views)
+(annot:enable-annot-syntax)
+
+;;; App definition
+
+(defapp app)
+
+;;; Views
+
+(defun get-user (req)
+  (let ((username (get-userid req)))
+    (when username
+      (single '<user> :username username))))
+
+@route app "/"
+(defview index ()
+  (if (logged-in-p req)
+      (render-template eco-template:timeline
+                       (get-user req)
+                       (user-timeline (get-user req)))
+      (render-template eco-template:index)))
+
+@route app "/profile/:username"
+(defview profile (username)
+  (let* ((user (single '<user> :username username))
+         (avatar-url (avatar-url user))
+         ;; The user's timeline
+         (user-tweets (user-tweets user))
+         ;; Is the user viewing his own profile?
+         (is-self (equal (get-userid req) username)))
+    (render-template eco-template:profile (get-userid req) user
+                     avatar-url user-tweets is-self)))
+
+@route app "/followers/:username"
+(defview user-followers (username)
+  (let ((user (single '<user> :username username)))
+    (render-template eco-template:user-list (get-userid req) "Followers"
+                     user (follower-summary user))))
+
+@route app "/following/:username"
+(defview user-following (username)
+  (let ((user (single '<user> :username username)))
+    (render-template eco-template:user-list (get-userid req) "Following"
+                     user (following-summary user))))
+
+;;; Authentication views
+
+@route app (:post "/signup")
+(defview sign-up ()
+  (with-params req (name username email password password-repeat)
+    (if (exists '<user> :username :username)
+        ;; Does a user with that name exist? In that case, render the landing
+        ;; template with a corresponding error
+        (render-template eco-template:index "A user with that name already exists.")
+        ;; We have a new user. Do both passwords match?
+        (if (equal password password-repeat)
+            ;; Okay, the passwords are a match. Let's create the user and return
+            ;; the user to the homepage
+            (progn
+              (register username name email password)
+              (redirect "/"))
+            ;; The passwords don't match
+            (render-template eco-template:index "Passwords don't match.")))))
+
+@route app (:post "/signin")
+(defview sign-in ()
+  (with-params req (username password)
+    ;; Check whether a user with this name exists
+    (let ((user (single '<user> :username username)))
+      (if user
+          (if (cl-pass:check-password password (password user))
+              (progn
+                ;; Log the user in
+                (login req username)
+                (redirect "/"))
+              ;; Wrong password
+              (render-template eco-template:index "Wrong password."))
+          ;; No such user
+          (render-template eco-template:index "No such user.")))))
+
+@route app (:get "/signout")
+(defview sign-out ()
+  (when (logged-in-p req)
+    (logout req))
+  (redirect "/"))
+
+;;; Bring it up
+
+(start app
+    :middleware ((clack.middleware.static:<clack-middleware-static>  
+                  :path "/static/"
+                  :root (asdf:system-relative-pathname :lucerne-utweet
+                                                       #p"examples/utweet/static/"))))
