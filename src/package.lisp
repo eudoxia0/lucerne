038c9ffd888b8e79078c6a3a0dae5bad4cab346d
Fernando Borretti <eudoxiahp@gmail.com>
2014-10-08 16:59:42 -0200

    Move package definition to src/package.lisp

---
+++ src/package.lisp
@@ -0,0 +1,29 @@
+(in-package :cl-user)
+(defpackage lucerne
+  (:use :cl :trivial-types :cl-annot :anaphora)
+  (:import-from :clack.util.route
+                :<url-rule>
+                :match
+                :make-url-rule)
+  (:import-from :clack.request
+                :make-request
+                :request-method
+                :request-uri
+                :parameter
+                :env)
+  (:export :<app>
+           :not-found
+           :add-route
+           :defview
+           :params
+           :req
+           :route
+           :defapp
+           :start
+           :stop
+           :respond
+           :redirect
+           :session
+           :with-params
+           :render-template))
+(in-package :lucerne)
