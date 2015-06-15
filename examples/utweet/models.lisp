d3280de8fe4723c2fb917660ba2adc9639d2628b
Fernando Borretti <eudoxiahp@gmail.com>
2015-06-14 21:13:28 -0300

    Rewrite the functions

--- examples/utweet/models.lisp
+++ examples/utweet/models.lisp
@@ -5,8 +5,6 @@
   (:export :register-user
            :followers
            :following
-           :follower-summary
-           :following-summary
            :tweet
            :user-timeline
            :user-tweets
@@ -16,8 +14,8 @@
 ;;; Models
 
 (defclass user ()
-  ((full-name :accessor user-full-name
-              :initarg :full-name
+  ((name :accessor user-name
+              :initarg :name
               :type string)
    (email :accessor user-email
           :initarg :email
@@ -32,7 +30,7 @@
 (defclass subscription ()
   ((follower :reader subscription-follower
              :initarg :follower
-             :type string
+             :type email
              :documentation "The follower's email.")
    (followed :reader subscription-followed
              :initarg :followed
@@ -51,61 +49,85 @@
               :initarg :timestamp
               :initform (local-time:now))))
 
-#|
-(defun register-user (username full-name email password)
-  "Register the user. Passwords are hashed."
-  (create '<user> :username username
-                  :full-name full-name
-                  :email email
-                  :password (cl-pass:hash password)
-                  ;; Get the user's Gravatar URL in 120px
-                  :avatar-url (avatar-api:gravatar email 120)))
+;;; Storage
+
+(defparameter *users* (make-hash-table :test #'equal))
+
+(defparameter *subscriptions* (list))
+
+(defparameter *tweets* (list))
+
+;;; Functions
+
+(defun find-user (email)
+  "Find a user by email address."
+  (gethash email *users*))
+
+(defun register-user (&key name email password)
+  "Register the user and hash their password."
+  (setf (gethash email *users*)
+        (make-instance 'user
+                       :name name
+                       :email email
+                       :password (cl-pass:hash password)
+                       :avatar-url (avatar-api:gravatar email 120))))
 
 (defun followers (user)
-  "List of IDs of users that follow `user`."
-  (loop for sub in (filter '<subscription> :target (id user)) collecting
-    (follower sub)))
+  "List of users that follow the user."
+  (mapcar #'(lambda (sub)
+              (find-user (subscription-follower sub)))
+          (remove-if-not #'(lambda (sub)
+                             (string= (subscription-followed sub)
+                                      (user-email user)))
+                         *subscriptions*)))
 
 (defun following (user)
-  "List of IDs of users followed by `user`."
-  (loop for sub in (filter '<subscription> :follower (id user)) collecting
-    (target sub)))
-
-(defun follower-summary (user)
-  "Username, full name, and avatar URL of `user`'s followers."
-  (query (select (:username :full-name :avatar-url)
-           (from '<user>)
-           (where (:in :id (followers user))))))
-
-(defun following-summary (user)
-  "Username, full name, and avatar URL of the users that follow `user`."
-  (query (select (:username :full-name :avatar-url)
-           (from '<user>)
-           (where (:in :id (following user))))))
+  "List of users the user follows."
+  (mapcar #'(lambda (sub)
+              (find-user (subscription-followed sub)))
+          (remove-if-not #'(lambda (sub)
+                             (string= (subscription-follower sub)
+                                      (user-email user)))
+                         *subscriptions*)))
 
 (defun tweet (author text)
-  (create '<tweet> :author (id author) :text text :date (local-time:now)))
+  (push (make-instance 'tweet
+                       :author (user-email author)
+                       :text text)
+        *tweets*))
+
+(defun sort-tweets (tweets)
+  "Given a list of tweets, sort them so the newest are first."
+  (sort tweets
+        #'(lambda (tweet-a tweet-b)
+            (local-time:timestamp>= (tweet-timestamp tweet-a)
+                                    (tweet-timestamp tweet-b)))))
 
 (defun user-timeline (user)
   "Find the tweets for this user's timeline."
-  (let* ((following-ids (following user))
-         ;; Here we use the SQL DSL instead of Crane's (intentionally) simple and
-         ;; limited interface.
-         (tweets (query (select :*
-                          (from '<tweet>)
-                          (where (:in :author following-ids))))))
-    (loop for tweet-plist in tweets collecting
-      (plist->object '<tweet> tweet-plist))))
+  (sort-tweets (remove-if-not #'(lambda (tweet)
+                                  (member (tweet-author tweet)
+                                          (following user)
+                                          :test #'equal))
+                              *tweets*)))
 
 (defun user-tweets (user)
   "Return a user's tweets, sorted through time"
-  (filter '<tweet> :author (id user)))
+  (sort-tweets (remove-if-not #'(lambda (tweet)
+                                  (string= (tweet-author tweet)
+                                           (user-email user)))
+                              *tweets*)))
 
-(defun follow (follower target)
-  (create '<subscription> :follower (id follower) :target (id target)))
+(defun follow (follower followed)
+  "Follow a user."
+  (push (make-instance 'subscription
+                       :follower (user-email follower)
+                       :followed (user-email followed))
+        *subscriptions*))
 
 ;;; Create some example data
 
+#|
 (let ((eudox (register-user "eudoxia" "eudoxia" "black.linen99@gmail.com" "pass"))
       (john  (register-user "john" "John Doe" "jdoe@initech.com" "test"))
       (jane  (register-user "jane" "Jane Doe" "jane.doe@initech.com" "test")))
