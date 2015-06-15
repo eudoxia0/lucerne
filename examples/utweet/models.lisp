80b7fb2fb605bfbe9f2e9abed4157a784d5e8530
Fernando Borretti <eudoxiahp@gmail.com>
2015-06-14 21:00:55 -0300

    Delete old utweet models

--- examples/utweet/models.lisp
+++ examples/utweet/models.lisp
@@ -1,23 +1,8 @@
 (in-package :cl-user)
 (defpackage utweet.models
-  (:use :cl :crane)
-  (:export ;; Class names and slots
-           :id
-           :<user>
-           :username
-           :full-name
-           :email
-           :password
-           :avatar-url
-           :<subscription>
-           :follower
-           :target
-           :<tweet>
-           :author
-           :date
-           :text
-           ;; Utility functions
-           :register-user
+  (:use :cl)
+  ;; Functions
+  (:export :register-user
            :followers
            :following
            :follower-summary
@@ -27,58 +12,46 @@
            :user-tweets
            :follow))
 (in-package :utweet.models)
-(annot:enable-annot-syntax)
-
-;;; Crane setup
-
-(setup
- :migrations-directory
- (asdf:system-relative-pathname :lucerne-utweet #p"examples/utweet/migrations/")
- :databases
- '(:main
-   (:type :sqlite3
-    :name ":memory:")))
-
-;; Since this is just an example app, we force Crane to create everything on
-;; every run by deleting the migrations
-(crane:delete-migrations t)
-
-(connect)
-
-;;; A quick extension to Crane: Handling timestamps
-
-(definflate (stamp 'timestamp)
-  ;; Inflate a database timestamp into a timestamp object
-  (local-time:parse-timestring stamp))
-
-(defdeflate (stamp local-time:timestamp)
-  ;; Deflate a timestamp object into a string
-  (local-time:format-timestring nil stamp))
 
 ;;; Models
 
-(deftable <user> ()
-  (username  :type text :uniquep t)
-  (full-name :type text)
-  (email     :type text)
-  (password  :type text)
-  (avatar-url :type text)
-  (:documentation "The user object. The `password` field contains a digest of
-  the password."))
+(defclass user ()
+  ((full-name :accessor user-full-name
+              :initarg :full-name
+              :type string)
+   (email :accessor user-email
+          :initarg :email
+          :type string)
+   (password :accessor user-password
+             :initarg :password
+             :type string)
+   (avatar-url :accessor user-avatar-url
+               :initarg :avatar-url
+               :type string)))
 
-(deftable <subscription> ()
-  (follower :type integer :foreign (<user>))
-  (target   :type integer :foreign (<user>))
-  (:documentation "Represents a user following another."))
+(defclass subscription ()
+  ((follower :reader subscription-follower
+             :initarg :follower
+             :type string
+             :documentation "The follower's email.")
+   (followed :reader subscription-followed
+             :initarg :followed
+             :type string
+             :documentation "The followed's email.")))
 
-(deftable <tweet> ()
-  (author :type integer :foreign (<user>))
-  (text :type text)
-  (date :type timestamp)
-  (:documentation "A tweet."))
+(defclass tweet ()
+  ((author :reader tweet-author
+           :initarg :author
+           :type string
+           :documentation "The author's email.")
+   (text :reader tweet-text
+         :initarg :text
+         :type string)
+   (timestamp :reader tweet-timestamp
+              :initarg :timestamp
+              :initform (local-time:now))))
 
-;;; Some utility functions
-
+#|
 (defun register-user (username full-name email password)
   "Register the user. Passwords are hashed."
   (create '<user> :username username
@@ -143,3 +116,4 @@
   (tweet john "Test message BEEP")
   (tweet jane "Test message BOOP")
   (tweet john "BEEP BOOP feed me followers"))
+|#
