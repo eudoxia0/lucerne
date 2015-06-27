aaa140936679f23df7650097f5b3474e931d02f9
Fernando Borretti <eudoxiahp@gmail.com>
2015-06-27 11:53:26 -0300

    Better skeleton system definition

--- skeleton/asdf.lisp
+++ skeleton/asdf.lisp
@@ -3,11 +3,23 @@
   :maintainer "{{#email}}{{author}} <{{email}}>{{/email}}{{^email}}{{author}}{{/email}}"
   {{#license}}:license "{{license}}"{{/license}}
   :version "0.1"
-  {{#ghuser}}:homepage "https://github.com/{{ghuser}}/{{name}}"
+  {{#ghuser}}
+  :homepage "https://github.com/{{ghuser}}/{{name}}"
   :bug-tracker "https://github.com/{{ghuser}}/{{name}}/issues"
   :source-control (:git "git@github.com:{{ghuser}}/{{name}}.git"){{/ghuser}}
   :depends-on ({{dependencies}})
-  :components ((:module "src"
+  {{#sassp}}
+  :defsystem-depends-on (:asdf-linguist)
+  {{/sassp}}
+  :components ((:module "assets"
+                :components
+                ((:module "css"
+                  :components
+                  ({{#sassp}}(:sass "style"){{/sassp}}{{^sassp}}(:static-file "style.css"){{/sassp}}))
+                 (:module "js"
+                  :components
+                  ((:static-file "scripts.js")))))
+               (:module "src"
                 :serial t
                 :components
                 ((:file "{{name}}"))))
