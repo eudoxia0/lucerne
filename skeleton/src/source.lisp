6020f649e448b13eb3c79ef308b081eda4e472ff
Fernando Borretti <eudoxiahp@gmail.com>
2015-06-27 11:53:36 -0300

    Add skeleton source

--- skeleton/src/source.lisp
+++ skeleton/src/source.lisp
@@ -1,4 +1,31 @@
 (in-package :cl-user)
 (defpackage {{name}}
-  (:use :cl))
+  (:use :cl :lucerne)
+  (:export :app)
+  (:documentation "Main {{name}} code."))
 (in-package :{{name}})
+
+;;; App
+
+(defapp app
+  :middlewares ((<clack-middleware-static>
+                 :root (asdf:system-relative-pathname :self #p"assets/build/")
+                 :path "/static/")))
+
+;;; Assets
+
+(rock:defenv :{{name}}
+  :assets ((:jquery :1.11.1))
+  :bundles ((:js
+            :assets ((:jquery :1.11.1))
+            :files (list #p"js/scripts.js")
+            :destination #p"js/scripts.js")
+            (:css
+             :assets ()
+             :files (list #p"css/style.css")
+             :destination #p"css/style.css")))
+
+;;; Templates
+
+
+;;; Views
