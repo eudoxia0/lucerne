0c7f5e2f7e53b086eafa90ca7cb15dfd4cdb1744
Fernando Borretti <eudoxiahp@gmail.com>
2015-07-02 23:37:12 -0300

    Add docstrings

--- contrib/auth/auth.lisp
+++ contrib/auth/auth.lisp
@@ -18,15 +18,19 @@
               :type     function)))
 
 (defun get-userid ()
+  "Extract the user ID from the current session."
   (gethash :userid (lucerne:session)))
 
 (defun login (userid)
+  "Log in the user specified by @cl:param(userid)."
   (setf (gethash :userid (lucerne:session))
         userid))
 
 (defun logout ()
+  "Log out the current user."
   (remhash :userid (lucerne:session)))
 
 (defun logged-in-p ()
+  "Whether the user is logged in or not."
   (if (get-userid)
       t))
